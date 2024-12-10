a76d3d8c00d76f4d23944218a594cdc33be81317
Jaz Thomson <jazthomson@gmail.com>
2024-12-10 18:49:32 +0000

    Solution for day 3 part 2

--- Day03.fs
+++ Day03.fs
@@ -16,7 +16,57 @@ module Part1 =
         |> Array.sumBy (fun (x,y) -> x * y)
 
 module Part2 =
-    let solver _ = 0
+    let getMulsWithIndex s =
+        s
+        |> Regex("""mul\((\d+),(\d+)\)""").Matches
+        |> Seq.toArray
+        |> Array.map (fun m -> int m.Groups[1].Value, int m.Groups[2].Value, m.Groups[0].Index)
+
+    let getDoIndexes s =
+        s
+        |> Regex("""do\(\)""").Matches
+        |> Seq.toArray
+        |> Array.map (fun m -> m.Captures.Item(0).Index)
+
+    let getDontIndexes s =
+        s
+        |> Regex("""don't\(\)""").Matches
+        |> Seq.toArray
+        |> Array.map (fun m -> m.Captures.Item(0).Index)
+
+    let solver input =
+        
+        let shouldInclude  (dos: int array) (donts: int array) (index: int) =
+            let lastDo =
+                dos
+                |> Array.tryFindBack (fun x -> x < index)
+                |> Option.defaultValue 0
+            
+            let lastDont =
+                donts
+                |> Array.tryFindBack (fun x -> x < index)
+                |> Option.defaultValue -1
+
+            lastDont < lastDo
+
+        // Sort is redundant, but just in case
+        let dos = input |> getDoIndexes |> Array.sort
+        let donts = input |> getDontIndexes |> Array.sort
+
+        input
+        |> getMulsWithIndex
+        |> Array.choose (fun (a, b, i) -> if shouldInclude dos donts i then Some (a * b) else None)
+        |> Array.sum
+
+        (*
+             x   ✔
+        1    -   0 = Some 1
+        28  20   0 = None
+        48  20   0 = None
+        64  20  59 = Some 64
+        
+        *)
+
 
 let part1 = {
     Day = 3
@@ -31,6 +81,7 @@ let part2 = {
     part1 with
         Part = 2
         Solver = Part2.solver
-        ExampleAnswer = -99
+        ExampleInput = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+        ExampleAnswer = 48
         ConfirmedAnswer = None
 }
